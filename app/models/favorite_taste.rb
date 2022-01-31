618ff767cf755c58a76e47ce2f57846841b9f9df
zaranah <zaranahier@gmail.com>
2022-02-01 00:09:30 +0900

    ユーザー管理機能の実装

--- app/models/favorite_taste.rb
+++ app/models/favorite_taste.rb
@@ -1,6 +1,6 @@
 class FavoriteTaste < ActiveHash::Base
   self.data = [
-    { id: 1, name: '--' },
+    { id: 1, name: '-Taste-' },
     { id: 2, name: '薄味派' },
     { id: 3, name: '普通' },
     { id: 4, name: '濃い味派' }
