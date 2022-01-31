62da3ed3fcde23e3aa717ed21ce0cf0852983c72
zaranah <zaranahier@gmail.com>
2022-01-31 15:56:47 +0900

    バリデーションの記入

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :favorite_taste_id, presence: true
 end
