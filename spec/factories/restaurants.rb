f42af7fe485886f81205c15ca13c40618673e00d
zaranah <zaranahier@gmail.com>
2022-02-01 19:02:35 +0900

    単体テストコードの記述

---
+++ spec/factories/restaurants.rb
@@ -0,0 +1,16 @@
+FactoryBot.define do
+  factory :restaurant do
+    name { Faker::Restaurant.name }
+    prefecture_id { Faker::Number.between(from: 2, to: 48) }
+    station { Faker::Quote.famous_last_words }
+    genre_id { Faker::Number.between(from: 2, to: 17) }
+    food { Faker::Food.dish }
+    price_id { Faker::Number.between(from: 2, to: 4) }
+    opinion { Faker::Quote.famous_last_words }
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
+  end
+end
