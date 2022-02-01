b5e9aa6b4ea17a220279a74e4f10cfe5e77e96b6
zaranah <zaranahier@gmail.com>
2022-02-01 15:47:26 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "restaurants#index"
   resources :users, only: [:edit, :update, :show]
+  resources :restaurants, only: [:new, :create]
 end
