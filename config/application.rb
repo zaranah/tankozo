b742a39de05b075883da1988b9de7ef2bafc62f5
zaranah <zaranahier@gmail.com>
2022-02-02 15:53:38 +0900

    日本時刻表示機能実装

--- config/application.rb
+++ config/application.rb
@@ -9,9 +9,9 @@ Bundler.require(*Rails.groups)
 module Tankozo
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.i18n.default_locale = :ja
-    config.time_zone = 'Tokyo'
     config.load_defaults 6.0
+    config.i18n.default_locale = :ja
+    config.time_zone = 'Asia/Tokyo'
     config.action_view.field_error_proc = Proc.new { |html_tag, instance| html_tag }
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
