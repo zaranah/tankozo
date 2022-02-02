e20fc9bd7ac60829a3b08289e817961e663c75cb
zaranah <zaranahier@gmail.com>
2022-02-02 17:49:36 +0900

    コメントテーブルの作成

--- db/migrate/20220202084002_create_comments.rb
+++ db/migrate/20220202084002_create_comments.rb
@@ -1,7 +1,9 @@
 class CreateComments < ActiveRecord::Migration[6.0]
   def change
     create_table :comments do |t|
-
+      t.string :comment,        null: false
+      t.references :user,       null: false, foreign_key: true
+      t.references :restaurant, null: false, foreign_key: true
       t.timestamps
     end
   end
