7a64a5e90964845faf662ce1e7a265e22f938b7c
zaranah <zaranahier@gmail.com>
2022-02-03 15:26:40 +0900

    ルーティングの記述

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "restaurants#index"
   resources :users, only: [:edit, :update, :show]
+
   resources :restaurants do
     resources :comments, only: :create
+    resources :likes, only: [:create, :destroy]
+    resources :hopes, only: [:create, :destroy]
   end
+  
 end
