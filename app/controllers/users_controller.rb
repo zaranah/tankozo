5d7c166920df82d72e3d9ff3235b5bb51917ee01
zaranah <zaranahier@gmail.com>
2022-02-05 13:25:47 +0900

    headerルートパス用たん小僧くん変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   before_action :authenticate_user!, only: :edit
   before_action :set_item, only: [:edit, :show, :update]
   before_action :move_to_index, only: :edit
+  before_action :header_item, only: [:edit, :show]
 
   def edit
   end
@@ -15,15 +16,8 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user_restaurants = @user.restaurants
     @user_hopes = @user.hopes
     @user_likes = @user.likes
-
-    @likes_count = 0
-    @user_restaurants.each do |restaurant|
-    @likes_count += restaurant.likes.count
-    end
-
   end
 
   private
@@ -34,6 +28,14 @@ class UsersController < ApplicationController
 
   def move_to_index
     redirect_to root_path unless current_user.id == @user.id
+  end
+
+  def header_item
+      @user_restaurants = @user.restaurants
+      @likes_count = 0
+      @user_restaurants.each do |restaurant|
+      @likes_count += restaurant.likes.count
+      end
   end
 
   def user_params
