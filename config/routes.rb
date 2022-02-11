e4e09fcf11345e2353c87f22bf040b7bd61ddc19
zaranah <zaranahier@gmail.com>
2022-02-11 11:34:40 +0900

    マイページ機能ルーティング記述

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "restaurants#index"
-  resources :users, only: [:edit, :update, :show]
+  resources :users, only: [:edit, :update, :show] do
+    resources :hopes, only: :index
+    resources :likes, only: :index
+    resources :users, only: :index
+  end
 
   resources :restaurants do
     resources :comments, only: :create
