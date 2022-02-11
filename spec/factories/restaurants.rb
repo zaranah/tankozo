813665f528b3ca0ccb05669053253674f024d89f
zaranah <zaranahier@gmail.com>
2022-02-11 17:20:04 +0900

    FactoryBotの追記

--- spec/factories/restaurants.rb
+++ spec/factories/restaurants.rb
@@ -1,6 +1,7 @@
 FactoryBot.define do
   factory :restaurant do
     name { Faker::Restaurant.name }
+    restaurant_url { Faker::Internet.url }
     prefecture_id { Faker::Number.between(from: 2, to: 48) }
     station { Faker::Quote.famous_last_words }
     genre_id { Faker::Number.between(from: 2, to: 17) }
