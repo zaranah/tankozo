c30fc48c74166bc72b77b683b0b46077e9fd9da7
zaranah <zaranahier@gmail.com>
2022-02-13 13:54:07 +0900

    コントローラーの記述

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -10,6 +10,13 @@ class CommentsController < ApplicationController
     render json: { comment: comment, user: user }
   end
 
+  def destroy
+    @restaurant = Restaurant.find(params[:restaurant_id]) 
+    @comments = @restaurant.comments.order('created_at DESC').includes(:user).page(params[:page]).per(20)
+    comment = Comment.find_by(id: params[:id], restaurant_id: params[:restaurant_id])
+    comment.destroy
+  end
+
   private
 
   def comment_params
