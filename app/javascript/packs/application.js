7d1578cd3e9e2fd145050cc18dd299312e660e11
zaranah <zaranahier@gmail.com>
2022-02-17 20:19:54 +0900

    トップスクロール実装

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,7 @@ require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
 require("../comment")
+require("../pagetop")
 import "bootstrap"
 import "../stylesheets/application"
 
