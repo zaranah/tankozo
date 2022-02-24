ceb4376d8e48f18903cc70bab982a4ba0ff53498
zaranah <zaranahier@gmail.com>
2022-02-24 11:17:25 +0900

    Basic認証解除

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,14 @@
 class ApplicationController < ActionController::Base
-  before_action :basic_auth
+  # before_action :basic_auth
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
 
-  def basic_auth
-    authenticate_or_request_with_http_basic do |username, password|
-      username == ENV['BASIC_AUTH_USER1'] && password == ENV['BASIC_AUTH_PASSWORD1']
-    end
-  end
+  # def basic_auth
+  #   authenticate_or_request_with_http_basic do |username, password|
+  #     username == ENV['BASIC_AUTH_USER1'] && password == ENV['BASIC_AUTH_PASSWORD1']
+  #   end
+  # end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :favorite_taste_id])
