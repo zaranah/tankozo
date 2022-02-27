1d9ac3f8397ed30c8d6c9f56e9d3c868a909efa2
zaranah <zaranahier@gmail.com>
2022-02-27 15:40:41 +0900

    restaurantモデルの記述の移行

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -10,25 +10,4 @@ class Restaurant < ApplicationRecord
   has_many :hopes, dependent: :destroy
   has_one_attached :image
 
-  validates :name, presence: true
-  validates :prefecture_id, numericality: { other_than: 1, message: 'を入力してください' }
-  validates :station, presence: true
-  validates :genre_id, numericality: { other_than: 1, message: 'を入力してください' }
-  validates :food, presence: true
-  validates :price_id, numericality: { other_than: 1, message: 'を入力してください' }
-  validates :opinion, presence: true
-
-  validates :image, presence: true
-  validate :file_type, if: :was_attached?
-
-  def was_attached?
-    image.attached?
-  end
-
-  def file_type
-    unless image.blob.content_type.in?(%('image/jpg image/jpeg image/png'))
-      errors.add(:image,
-                 'は JPG/JPEG 形式または PNG 形式のみ選択してください')
-    end
-  end
 end
