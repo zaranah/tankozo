59979b0c93138b94764fb8b237437e8adf0ced09
zaranah <zaranahier@gmail.com>
2022-03-01 16:11:53 +0900

    タグバリデーションの強化

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,5 +1,5 @@
 class Tag < ApplicationRecord
   has_many :restaurant_tag_relations
   has_many :restaurants, through: :restaurant_tag_relations
-  validates :tag_name, uniqueness: true
+  validates :tag_name, uniqueness: { case_sensitive: true }
 end
