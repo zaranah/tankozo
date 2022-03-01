c94881014366aa40f3200293bc240cf69d17d7a1
zaranah <zaranahier@gmail.com>
2022-03-01 12:00:55 +0900

    Factory Botの記述変更

--- spec/factories/restaurants.rb
+++ spec/factories/restaurants.rb
@@ -1,17 +1,17 @@
-FactoryBot.define do
-  factory :restaurant do
-    name { Faker::Restaurant.name }
-    restaurant_url { Faker::Internet.url }
-    prefecture_id { Faker::Number.between(from: 2, to: 48) }
-    station { Faker::Quote.famous_last_words }
-    genre_id { Faker::Number.between(from: 2, to: 17) }
-    food { Faker::Food.dish }
-    price_id { Faker::Number.between(from: 2, to: 4) }
-    opinion { Faker::Quote.famous_last_words }
-    association :user
+# FactoryBot.define do
+#   factory :restaurant do
+#     name { Faker::Restaurant.name }
+#     restaurant_url { Faker::Internet.url }
+#     prefecture_id { Faker::Number.between(from: 2, to: 48) }
+#     station { Faker::Quote.famous_last_words }
+#     genre_id { Faker::Number.between(from: 2, to: 17) }
+#     food { Faker::Food.dish }
+#     price_id { Faker::Number.between(from: 2, to: 4) }
+#     opinion { Faker::Quote.famous_last_words }
+#     association :user
 
-    after(:build) do |item|
-      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
-    end
-  end
-end
+#     after(:build) do |item|
+#       item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+#     end
+#   end
+# end
