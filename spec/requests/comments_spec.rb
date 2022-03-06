ea6dc36c9dcfb34b23af04532a451631576ecf1f
zaranah <zaranahier@gmail.com>
2022-03-06 23:28:40 +0900

    コメントコントローラーテスト記述

--- spec/requests/comments_spec.rb
+++ spec/requests/comments_spec.rb
@@ -1,7 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe 'Comments', type: :request do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+  before do
+    @restaurant = FactoryBot.create(:restaurant)
+    @comment = FactoryBot.create(:comment)
+  end
+
+  describe 'POST #create' do
+    it 'createアクションにリクエストすると正常にレスポンスが返ってくる' do 
+      sign_in @restaurant.user
+      get "/restaurants/#{@restaurant.id}", params: { restaurant_id: @restaurant }
+      post "/restaurants/#{@restaurant.id}/comments", :params => { restaurant_id: @restaurant, :comment => { :comment => "test"}}
+      expect(response.status).to eq 200
+    end
+  end
+
+  describe 'DELETE #destroy' do
+    it 'destroyアクションにリクエストすると正常にレスポンスが返ってくる' do 
+      delete "/restaurants/#{@restaurant.id}/comments/#{@comment.id}"
+      get "/restaurants/#{@restaurant.id}", params: { restaurant_id: @restaurant }
+      expect(response.status).to eq 200
+    end
+    it 'ログアウト状態でdestroyアクションにリクエストすると異常のレスポンスが返ってくる' do
+      get "/restaurants/#{@restaurant.id}", params: { restaurant_id: @restaurant }
+      delete "/restaurants/#{@restaurant.id}/comments/#{@comment.id}", params: { restaurant_id: @restaurant, comment_id: @comment }
+      expect(response.status).to eq 302
+    end
   end
 end
