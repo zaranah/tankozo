88f1f7368e679c679a368cb174021400a5b2ad76
zaranah <zaranahier@gmail.com>
2022-03-07 12:22:05 +0900

    タグコントローラーテスト記述

--- spec/requests/tags_spec.rb
+++ spec/requests/tags_spec.rb
@@ -1,7 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe 'Tags', type: :request do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+  before do
+    @tag = FactoryBot.create(:tag)
+  end
+
+  describe 'GET #show' do
+    it 'showアクションにリクエストすると正常にレスポンスが返ってくる' do
+      get "/tags/#{@tag.id}", params: { tag_id: @tag }
+      expect(response.status).to eq 200
+    end
+    it 'showアクションにリクエストするとレスポンスにタグ名が存在する' do 
+      get "/tags/#{@tag.id}", params: { tag_id: @tag }
+      expect(response.body).to include(@tag.tag_name)
+    end
   end
 end
