2d83685e8096ec3262a8540ad9f4aa1da844d51b
zaranah <zaranahier@gmail.com>
2022-03-08 23:44:11 +0900

    likes結合テスト修正

--- spec/system/likes_spec.rb
+++ spec/system/likes_spec.rb
@@ -7,18 +7,13 @@ RSpec.describe "Likes", type: :system do
   end
 
   describe 'create' do
-    it 'ログインしたユーザーは自分が投稿していない店舗詳細ページにて「行きたい」・「よかった」ボタンをクリックできる' do
+    it 'ログインしたユーザーは自分が投稿していない店舗詳細ページにて「よかった」ボタンをクリックできる' do
       # 店舗１を投稿したユーザーでログインする
       sign_in_support(@restaurant1.user)
       # 詳細ページに遷移する
       visit restaurant_path(@restaurant2)
-      # 店舗投稿内に「行きたい」・「よかった」ボタンがあることを確認する
-      expect(page).to have_content('行きたい' && 'よかった')
-      # 店舗投稿内の「行きたい」ボタンを正常にクリックできることを確認する
-      expect do
-        find('.fa-running').click
-        visit restaurant_path(@restaurant2)
-      end.to change { Hope.count }.by(1)
+      # 店舗投稿内に「よかった」ボタンがあることを確認する
+      expect(page).to have_content('よかった')
       # 店舗投稿内の「よかった」ボタンを正常にクリックできることを確認する
       expect do
         find('.fa-heart').click
@@ -26,22 +21,19 @@ RSpec.describe "Likes", type: :system do
       end.to change { Like.count }.by(1)
     end
 
-    it 'ログインしたユーザーは自分が投稿した店舗詳細ページに「行きたい」・「よかった」ボタンが表示されない' do
+    it 'ログインしたユーザーは自分が投稿した店舗詳細ページに「よかった」ボタンが表示されない' do
       # 店舗１を投稿したユーザーでログインする
       sign_in_support(@restaurant1.user)
       # 詳細ページに遷移する
       visit restaurant_path(@restaurant1)
-      # 店舗投稿内に「行きたい」・「よかった」ボタンがないことを確認する
-      expect(page).to have_no_content('行きたい' && 'よかった')
+      # 店舗投稿内に「よかった」ボタンがないことを確認する
+      expect(page).to have_no_content('よかった')
     end
 
-    it 'ログインしていないユーザーは自分が投稿した店舗詳細ページに「行きたい」・「よかった」ボタンが押せない' do
+    it 'ログインしていないユーザーは自分が投稿した店舗詳細ページに「よかった」ボタンが押せない' do
       # 詳細ページに遷移する
       visit restaurant_path(@restaurant1)
-      # 店舗投稿内に「行きたい」・「よかった」ボタンを押せないことを確認する
-      expect do
-        find('.fa-running').click
-      end.not_to change { Hope.count }
+      # 店舗投稿内「よかった」ボタンを押せないことを確認する
       expect do
         find('.fa-heart').click
       end.not_to change { Like.count }
@@ -49,28 +41,18 @@ RSpec.describe "Likes", type: :system do
   end
 
   describe 'destroy' do
-    it 'ログインしたユーザーは自分が投稿していない店舗詳細ページにて「行きたい」・「よかった」ボタンを消すことができる' do
+    it 'ログインしたユーザーは自分が投稿していない店舗詳細ページにて「よかった」ボタンを消すことができる' do
       # 店舗１を投稿したユーザーでログインする
       sign_in_support(@restaurant1.user)
       # 詳細ページに遷移する
       visit restaurant_path(@restaurant2)
-      # 店舗投稿内に「行きたい」・「よかった」ボタンがあることを確認する
-      expect(page).to have_content('行きたい' && 'よかった')
-      # 店舗投稿内の「行きたい」ボタンを正常にクリックできることを確認する
-      expect do
-        find('.fa-running').click
-        visit restaurant_path(@restaurant2)
-      end.to change { Hope.count }.by(1)
+      # 店舗投稿内に「よかった」ボタンがあることを確認する
+      expect(page).to have_content('よかった')
       # 店舗投稿内の「よかった」ボタンを正常にクリックできることを確認する
       expect do
         find('.fa-heart').click
         visit restaurant_path(@restaurant2)
       end.to change { Like.count }.by(1)
-      # 店舗投稿内の「行きたい」ボタンをもう一度押すと削除できることを確認する
-      expect do
-        find('.fa-running').click
-        visit restaurant_path(@restaurant2)
-      end.to change { Hope.count }.by(-1)
       # 店舗投稿内の「よかった」ボタンをもう一度押すと削除できることを確認する
       expect do
         find('.fa-heart').click
