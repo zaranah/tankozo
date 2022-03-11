846cb5feba865364379b7b3c81a02002caeb212a
zaranah <zaranahier@gmail.com>
2022-03-12 00:09:25 +0900

    ユーザー編集のコントローラー変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,7 +22,7 @@ class UsersController < ApplicationController
 
   def update
     if current_user.update(user_params)
-      redirect_to root_path
+      redirect_to user_path(@user)
     else
       render :edit
     end
