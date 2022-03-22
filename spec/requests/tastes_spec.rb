f3ac115e4c8302ee21a377c628cf9fb50b48faf4
zaranah <zaranahier@gmail.com>
2022-03-22 18:44:10 +0900

    tastesコントローラーテスト記述

--- spec/requests/tastes_spec.rb
+++ spec/requests/tastes_spec.rb
@@ -1,7 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe 'Tastes', type: :request do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+  before do
+    @user = FactoryBot.create(:user)
+  end
+
+  describe 'GET #show' do
+    it 'showアクションにリクエストすると正常にレスポンスが返ってくる' do
+      get taste_path(@user.favorite_taste_id)
+      expect(response.status).to eq 200
+    end
+    it 'showアクションにリクエストするとレスポンスに味の好みが存在する' do
+      get "/tastes/#{@user.favorite_taste_id}", params: { favorite_taste_id: @user.favorite_taste_id }
+      expect(response.body).to include(@user.favorite_taste.name)
+    end
   end
 end
